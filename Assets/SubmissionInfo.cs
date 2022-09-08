9035b71e44a4bc3ee45b4295d3661a4ee0440d14
Minethrower <102025260+Minethrower@users.noreply.github.com>
2022-09-08 18:38:22 +1000

    init - Milestone 1

--- Assets/SubmissionInfo.cs
+++ Assets/SubmissionInfo.cs
@@ -9,19 +9,18 @@ public static class SubmissionInfo
     // for extra details related to use of this file.
 
     // URL to your group's project 2 repository on GitHub.
-    public static readonly string RepoURL = "https://github.com/COMP30019/project-2-";
+    public static readonly string RepoURL = "https://github.com/COMP30019/project-2-rogue-lab";
     
     // Come up with a team name below (plain text, no more than 50 chars).
-    public static readonly string TeamName = "Replace with your team name";
+    public static readonly string TeamName = "Rogue Lab";
     
     // List every team member below. Ensure student names/emails match official
     // UniMelb records exactly (e.g. avoid nicknames or aliases).
     public static readonly TeamMember[] Team = new[]
     {
-        new TeamMember("Student Name 1", "...@student.unimelb.edu.au"),
+        new TeamMember("Changmin Lee", "changminl@student.unimelb.edu.au"),
         new TeamMember("Student Name 2", "...@student.unimelb.edu.au"),
         new TeamMember("Student Name 3", "...@student.unimelb.edu.au"),
-        // Remove the following line if you have a group of 3
         new TeamMember("Student Name 4", "...@student.unimelb.edu.au"), 
     };
 
