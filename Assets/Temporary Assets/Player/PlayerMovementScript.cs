06a50b8b5bba1ada21b94745cd5943882365c934
Minethrower <102025260+Minethrower@users.noreply.github.com>
2022-10-04 00:45:06 +1100

    add updateDelay/getInput, guard init

--- Assets/Temporary Assets/Player/PlayerMovementScript.cs
+++ Assets/Temporary Assets/Player/PlayerMovementScript.cs
@@ -10,12 +10,19 @@ public class PlayerMovementScript : MonoBehaviour
     public Transform ShootLoc;
     public Transform Head;
     public HealthBar HealthBar;
-    private float Health=100.0f;
+    private float Health = 100.0f;
     public float MaxHealth = 200.0f;
     //public Transform PlayerTransform;
+
+    private int fireDelay = 0;
+    private int fireSpeed = 100;
+    
+    private bool guarding = false; // hold right mouse to guard
+
     public void DecreaseHealth(float damage) 
     {
         //Debug.Log("e");
+        
         Health -= damage;
         if (Health > MaxHealth) { Health = MaxHealth; }
         else if (Health < 0) { Debug.Log("Implement Dying here"); }
@@ -28,19 +35,38 @@ public class PlayerMovementScript : MonoBehaviour
         //PlayerTransform = this.GetComponent<Transform>();
     }
 
+    void updateDelay(){
+        fireDelay --;
+        if (fireDelay < 0){
+            fireDelay = 0;
+        }
+    }
+
+    void getInput(){
+        if (Input.GetButton("Fire1") && fireDelay <= 0)
+        {
+             //Instantiate(Projectile, new Vector3(0, 1, 0), Quaternion.identity);
+            fireDelay = fireSpeed;
+            GameObject o = Instantiate(Projectile, ShootLoc.position, Quaternion.identity);
+            o.GetComponent<Rigidbody>().velocity = 10.0f*(ShootLoc.position - Head.position);
+            //Debug.Log(Input.mousePosition);
+            Debug.Log(fireDelay);
+        }
+        Debug.Log(guarding);
+        if (Input.GetButton("Fire2")){
+            guarding = true;
+        }
+        else {
+            guarding = false;
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-           
-             //Instantiate(Projectile, new Vector3(0, 1, 0), Quaternion.identity);
-            GameObject o = Instantiate(Projectile, ShootLoc.position, Quaternion.identity);
-            o.GetComponent<Rigidbody>().velocity = 10.0f*(ShootLoc.position - Head.position);
-            Debug.Log(Input.mousePosition);
-            
-        }
-        
+        updateDelay();
+        getInput();
+
         Ray ShootLocation = PlayerCamera.ScreenPointToRay(Input.mousePosition);
         float al = ShootLocation.direction.y;//Vector3.Dot(ShootLocation.direction, new Vector3(0.0f,1.0f,0.0f));
         float val = Head.position.y - ShootLocation.origin.y;//(PlayerTransform.position - PlayerCamera.transform.position).y; //(center - .Origin).Dot(normal);
@@ -50,10 +76,10 @@ public class PlayerMovementScript : MonoBehaviour
         Head.LookAt(LookLoc, new Vector3(0.0f,1.0f,0.0f));
         //Vector3 Left = new Vector3( 5.0f, 0.0f, 0.0f );
         //Vector3 Forward = new Vector3( 0.0f, 0.0f, 5.0f ); 
-        Vector3 Left = new Vector3( 5.0f, 0.0f, -5.0f );
-        Vector3 Forward = new Vector3( 5.0f, 0.0f, 5.0f );
+        Vector3 Left = guarding?(new Vector3( 1.5f, 0.0f, -1.5f )):(new Vector3( 5.0f, 0.0f, -5.0f ));
+        Vector3 Forward = guarding?(new Vector3( 1.5f, 0.0f, 1.5f )):(new Vector3( 5.0f, 0.0f, 5.0f ));
 
         // Player.AddForce( Input.GetAxis("Horizontal")*Left +  Input.GetAxis("Vertical")*Forward);
-        Player.velocity = new Vector3(0.0f,Player.velocity.y,0.0f)+Input.GetAxis("Horizontal") * Left + Input.GetAxis("Vertical") * Forward;
+        Player.velocity = new Vector3(0.0f,Player.velocity.y,0.0f)+ Input.GetAxis("Horizontal") * Left + Input.GetAxis("Vertical") * Forward;
     }
 }
